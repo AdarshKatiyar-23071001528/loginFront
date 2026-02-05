131d6164b33a1aa88bacad327131955daf02e5e5
Adarsh <adarshkatiyar73@gmail.com>
2026-02-05 06:13:41 +0530

    baseUrl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL:"https://loginback.vercel.app/api/user"
-    baseURL: "http://localhost:1000/api/user"
+    baseURL:"https://loginback.vercel.app/api/user"
+    // baseURL: "http://localhost:1000/api/user"
 });
 
 export default api;
